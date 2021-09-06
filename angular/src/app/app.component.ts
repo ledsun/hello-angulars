cf15350a4333c74a4e593e8dfeb59f0aa0b4ea84
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2021-09-06 10:15:52 +0900

    AngularJSの起動をやめて、echo Angularアプリケーションだけを起動します

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  selector: 'echo',
   template: `
   <input #box (keyup)="0">
   {{box.value}}`
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,26 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { UpgradeModule } from '@angular/upgrade/static';
 import { AppComponent } from './app.component';
-import { downgradeComponent, getAngularJSGlobal } from '@angular/upgrade/static';
-
-const angular = getAngularJSGlobal()
-angular.module('angularjs',[])
-  .directive(
-    'echo',
-    downgradeComponent({ component: AppComponent })
-  );
 
 @NgModule({
+  declarations: [
+    AppComponent
+  ],
   imports: [
-    BrowserModule,
-    UpgradeModule
-  ]
+    BrowserModule
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
-  constructor(private upgrade: UpgradeModule) { }
-  ngDoBootstrap() {
-    this.upgrade.bootstrap(document.body, ['angularjs']);
-  }
-}
+export class AppModule { }
 
