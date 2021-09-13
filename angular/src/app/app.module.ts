eaa35bfd2575f27d5ff49b877d6b9c9ba631cfc0
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2021-09-13 11:26:43 +0900

    UpgradeModuleを読み込みます

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { UpgradeModule } from '@angular/upgrade/static'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UpgradeModule
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private upgrade: UpgradeModule) { }
+}
 
